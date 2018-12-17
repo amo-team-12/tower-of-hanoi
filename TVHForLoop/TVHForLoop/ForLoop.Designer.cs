5b3a1775637aaf7722d9577dd6732ea5eba5fb03
Unknown <glenn.dev@hotmail.com>
2018-12-17 13:45:37 +0100

    CHanged Names of labels, etc

--- TVHForLoop/TVHForLoop/ForLoop.Designer.cs
+++ TVHForLoop/TVHForLoop/ForLoop.Designer.cs
@@ -28,76 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.timeOutputLabel = new System.Windows.Forms.Label();
+            this.layerInput = new System.Windows.Forms.TextBox();
+            this.layerAskLabel = new System.Windows.Forms.Label();
+            this.movesOutputLabel = new System.Windows.Forms.Label();
+            this.towerOfHanoiPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.towerOfHanoiPicture)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // timeOutputLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 63);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Tijd:";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.timeOutputLabel.AutoSize = true;
+            this.timeOutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeOutputLabel.Location = new System.Drawing.Point(12, 63);
+            this.timeOutputLabel.Name = "timeOutputLabel";
+            this.timeOutputLabel.Size = new System.Drawing.Size(37, 20);
+            this.timeOutputLabel.TabIndex = 0;
+            this.timeOutputLabel.Text = "Tijd:";
+            this.timeOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBox1
+            // layerInput
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(143, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(414, 26);
-            this.textBox1.TabIndex = 1;
+            this.layerInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.layerInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.layerInput.Location = new System.Drawing.Point(143, 6);
+            this.layerInput.Name = "layerInput";
+            this.layerInput.Size = new System.Drawing.Size(414, 26);
+            this.layerInput.TabIndex = 1;
             // 
-            // label2
+            // layerAskLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Hoeveel Lagen?";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.layerAskLabel.AutoSize = true;
+            this.layerAskLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.layerAskLabel.Location = new System.Drawing.Point(12, 9);
+            this.layerAskLabel.Name = "layerAskLabel";
+            this.layerAskLabel.Size = new System.Drawing.Size(125, 20);
+            this.layerAskLabel.TabIndex = 2;
+            this.layerAskLabel.Text = "Hoeveel Lagen?";
+            this.layerAskLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label3
+            // movesOutputLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 43);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 20);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Zetten:";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.movesOutputLabel.AutoSize = true;
+            this.movesOutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.movesOutputLabel.Location = new System.Drawing.Point(12, 43);
+            this.movesOutputLabel.Name = "movesOutputLabel";
+            this.movesOutputLabel.Size = new System.Drawing.Size(60, 20);
+            this.movesOutputLabel.TabIndex = 3;
+            this.movesOutputLabel.Text = "Zetten:";
+            this.movesOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // pictureBox1
+            // towerOfHanoiPicture
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(194, 43);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(363, 132);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            this.towerOfHanoiPicture.Location = new System.Drawing.Point(194, 43);
+            this.towerOfHanoiPicture.Name = "towerOfHanoiPicture";
+            this.towerOfHanoiPicture.Size = new System.Drawing.Size(363, 132);
+            this.towerOfHanoiPicture.TabIndex = 4;
+            this.towerOfHanoiPicture.TabStop = false;
             // 
             // ForLoop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(567, 187);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.towerOfHanoiPicture);
+            this.Controls.Add(this.movesOutputLabel);
+            this.Controls.Add(this.layerAskLabel);
+            this.Controls.Add(this.layerInput);
+            this.Controls.Add(this.timeOutputLabel);
             this.Name = "ForLoop";
             this.Text = "Tower of Hanoi Calculator";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.towerOfHanoiPicture)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,11 +107,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label timeOutputLabel;
+        private System.Windows.Forms.TextBox layerInput;
+        private System.Windows.Forms.Label layerAskLabel;
+        private System.Windows.Forms.Label movesOutputLabel;
+        private System.Windows.Forms.PictureBox towerOfHanoiPicture;
     }
 }
 
